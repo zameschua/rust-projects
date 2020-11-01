921458d3a02e8e5e1a2d2f1d4801aaf587f7bdf2
zameschua <zames.chua@gmail.com>
2020-11-01 17:15:19 +0800

    Implement tests

--- kv_store/src/store.rs
+++ kv_store/src/store.rs
@@ -11,6 +11,8 @@ pub struct Store {
 
 impl Store {
   // Errors from this method are propogated up the stack
+  // Rehydrate a store from the file specified at the path,
+  // Creating it if it doesn't exist
   pub fn rehydrate_from(log_file_name: &str) -> Result<Store, ()> {
     let mut hashmap: HashMap<String, String> = HashMap::new();
 
@@ -44,7 +46,6 @@ impl Store {
   pub fn set(&mut self, key: &str, value: &str) -> Result<(), io::Error> {
     // 1. Write into the write-ahead log
     let file: File = fs::OpenOptions::new()
-    .write(true)
     .append(true)
     .create(true)
     .open(&self.log_file_name)
@@ -59,13 +60,17 @@ impl Store {
 }
 
 // Helper method to read contents of file by line and return an iterator over the lines
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+// Create the file if it doesn't exist, open it if it exists
+fn read_lines<P>(log_file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
-    let file: File = File::open(filename)?;
+    let file: File = fs::OpenOptions::new()
+    .read(true)
+    .append(true)
+    .create(true)
+    .open(log_file_name)
+    .unwrap();
     Ok(io::BufReader::new(file).lines())
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -73,28 +78,51 @@ mod tests {
 
   #[test]
   fn store_rehydrate_success() {
-    const TEST_LOG_FILE_NAME: &str = "kv_log_tests_success.csv";
-    let mut store: Store = match Store::rehydrate_from(TEST_LOG_FILE_NAME) {
-      Ok(store) => store,
-      Err(_) => panic!("Failed to rehydrate store")
-    };
+    const TEST_LOG_FILE_NAME: &str = "./tests/kv_log_tests_success.csv";
+    Store::rehydrate_from(TEST_LOG_FILE_NAME).expect("Failed to rehydrate store");
   }
 
   #[test]
-  fn store_rehydrate_failure() {
-    const TEST_LOG_FILE_NAME: &str = "kv_log_tests_failure.csv";
-    let mut store: Store = match Store::rehydrate_from(TEST_LOG_FILE_NAME) {
-      Ok(store) => store,
-      Err(_) => panic!("Failed to rehydrate store")
-    };
+  #[should_panic]
+  fn store_rehydrate_failure_should_panic() {
+    const TEST_LOG_FILE_NAME: &str = "./tests/kv_log_tests_failure.csv";
+    Store::rehydrate_from(TEST_LOG_FILE_NAME).expect("Failed to rehydrate store");
   }
 
   #[test]
   fn store_rehydrate_non_existant_file() {
-    const TEST_LOG_FILE_NAME: &str = "non_existant_file.csv";
-    let mut store: Store = match Store::rehydrate_from(TEST_LOG_FILE_NAME) {
-      Ok(store) => store,
-      Err(_) => panic!("Failed to rehydrate store")
-    };
+    const TEST_LOG_FILE_NAME: &str = "./tests/non_existant_file.csv";
+    Store::rehydrate_from(TEST_LOG_FILE_NAME).expect("Failed to rehydrate store");
+
+    // Cleanup
+    fs::remove_file(TEST_LOG_FILE_NAME).expect("Failed to remove file after running tests!")
+  }
+
+  #[test]
+  fn store_get_nonexistent_key_should_handle_gracefully() {
+    const TEST_LOG_FILE_NAME: &str = "./tests/temp_file.csv";
+    let store = Store::rehydrate_from(TEST_LOG_FILE_NAME).expect("Failed to rehydrate store");
+    
+    store.get("nonexistent-key-123");
+
+    // Cleanup
+    fs::remove_file(TEST_LOG_FILE_NAME).expect("Failed to remove file after running tests!")
+  }
+
+  #[test]
+  fn store_set_and_get_success() {
+    const TEST_LOG_FILE_NAME: &str = "./tests/temp_file.csv";
+    let mut store = Store::rehydrate_from(TEST_LOG_FILE_NAME).expect("Failed to rehydrate store");
+    
+    let key = "key";
+    let value = "value";
+    store.set(key, value).unwrap();
+    let retrieved_value = store.get(key).unwrap();
+
+    // Cleanup
+    fs::remove_file(TEST_LOG_FILE_NAME).expect("Failed to remove file after running tests!");
+
+
+    assert_eq!(value, retrieved_value);
   }
 }
